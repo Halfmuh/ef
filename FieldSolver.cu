3ee9dd9480f0376a00906fc1ab538ac591a4b649
Zuev Mikhail <halfmuh@gmail.com>
2019-01-28 23:23:14 +0700

    single iteration

--- FieldSolver.cu
+++ FieldSolver.cu
@@ -228,7 +228,7 @@ void FieldSolver::eval_potential(Inner_regions_manager &inner_regions)
 
 void FieldSolver::solve_poisson_eqn_Jacobi(Inner_regions_manager &inner_regions)
 {
-	max_Jacobi_iterations = 150;
+	max_Jacobi_iterations = 1;
 	int iter;
 
 	for (iter = 0; iter < max_Jacobi_iterations; ++iter) {
